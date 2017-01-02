8d21285626e9b71085913377b5766678ea479c2e
satoshicano <satoshicano@gmail.com>
2017-01-02 20:50:33 +0900

    localization to japanese

--- config/application.rb
+++ config/application.rb
@@ -15,6 +15,7 @@ Bundler.require(*Rails.groups)
 module Jointhub
   class Application < Rails::Application
     config.action_view.field_error_proc = proc { |html_tag, instance| html_tag }
+    config.i18n.default_locale = :ja
     config.time_zone = 'Asia/Tokyo'
 
     config.generators do |g|
