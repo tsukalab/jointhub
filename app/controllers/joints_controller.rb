8549ccfddd921fca042daa5aba78b120d778b2ba
satoshicano <satoshicano@gmail.com>
2017-02-17 18:21:41 +0900

    limit actions without login

--- app/controllers/joints_controller.rb
+++ app/controllers/joints_controller.rb
@@ -1,4 +1,5 @@
 class JointsController < ApplicationController
+  before_action :authenticate_user!, only: [:edit, :update, :destroy]
   before_action :set_joint, only: [:show, :edit, :update, :destroy]
 
   # GET /joints
