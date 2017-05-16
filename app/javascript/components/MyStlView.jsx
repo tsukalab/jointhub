395329559b4c5233b40e059581b81d1433b7221f
satoshicano <satoshicano@gmail.com>
2017-05-17 03:45:45 +0900

    set props

--- app/javascript/components/MyStlView.jsx
+++ app/javascript/components/MyStlView.jsx
@@ -4,11 +4,11 @@ import STLViewer from 'stl-viewer'
 export default function MyStlView(props) {
   return <STLViewer
     url={props.url}
-    width={400}
-    height={400}
+    width={props.width}
+    height={props.height}
     modelColor='#B92C2C'
     backgroundColor='#EAEAEA'
-    rotate={true}
-    orbitControls={true}
+    rotate={props.rotate}
+    orbitControls={props.orbitControls}
     />;
 }
