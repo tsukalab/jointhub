2a52d83b29f89856a009b527ef09b738d6aadcc6
satoshicano <satoshicano@gmail.com>
2017-05-17 17:02:51 +0900

    update header

--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,7 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {
     omniauth_callbacks: 'users/omniauth_callbacks'
   }
+  direct :github do
+    'https://github.com/tsukalab/jointhub'
+  end
 end
